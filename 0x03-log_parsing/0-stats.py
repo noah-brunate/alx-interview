600f6022b56c8824764656795ee736c31a9d3f04
noah-brunate <noahbrunate9@gmail.com>
2023-10-19 23:01:11 -0700

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,11 +5,11 @@ Input format:
 <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
 <status code> <file size>
 """
-
 import sys
 
 
-status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+               '404': 0, '405': 0, '500': 0}
 file_size = 0
 count = 0
 
@@ -18,21 +18,21 @@ try:
     for line in sys.stdin:
         arg_list = line.split(" ")
 
-    if len(arg_list) > 4:
-        size = int(arg_list[-1])
-        status = arg_list[-2]
+        if len(arg_list) > 4:
+            size = int(arg_list[-1])
+            status = arg_list[-2]
 
-        if status in status_dict.keys():
-            status_dict[status] += 1
+            if status in status_dict.keys():
+                status_dict[status] += 1
 
-        file_size += size
-        count += 1
-    if count == 10:
-        count = 0
-        print(f"File size: {file_size}")
-        for key, val in sorted(status_dict.items()):
-            if val != 0:
-                print(f"{key}: {val}")
+            file_size += size
+            count += 1
+        if count == 10:
+            count = 0
+            print(f"File size: {file_size}")
+            for key, val in sorted(status_dict.items()):
+                if val != 0:
+                    print(f"{key}: {val}")
 
 except Exception as e:
     pass
