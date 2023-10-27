7a57622a884d942cbf9bdc4a7413686f054cbd88
noah-brunate <noahbrunate9@gmail.com>
2023-10-27 02:39:21 -0700

    utf-8 validation code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,16 +15,14 @@ def validUTF8(data):
 
     for num in data:
         if num_bytes == 0:
-            if (num >> 7) == 0b0:
-                continue
+            if (num >> 7) != 0b0:
+                return False
             elif (num >> 5) == 0b110:
                 num_bytes = 1
             elif (num >> 4) == 0b1110:
                 num_bytes = 2
             elif (num >> 3) == 0b11110:
                 num_bytes = 3
-            else:
-                return False
         else:
             if (num >> 6) != 0b10:
                 return False
