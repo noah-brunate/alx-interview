05897ecd7f58d0ba7840c0fe069cb5e4c3957c8d
noah-brunate <noahbrunate9@gmail.com>
2023-10-27 02:45:04 -0700

    utf-8 validation code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,16 +15,23 @@ def validUTF8(data):
 
     for num in data:
         if num_bytes == 0:
-            if (num >> 7) != 0b0:
-                return False
+            if (num >> 7) == 0b0:
+                continue
             elif (num >> 5) == 0b110:
                 num_bytes = 1
             elif (num >> 4) == 0b1110:
                 num_bytes = 2
             elif (num >> 3) == 0b11110:
                 num_bytes = 3
+            else:
+                return False
         else:
             if (num >> 6) != 0b10:
                 return False
+
+            # num_bytes resets to 0, when all
+            # bytes are looped over
             num_bytes -= 1
+
+    # return True if num_bytes is 0 at end of the loop
     return num_bytes == 0
