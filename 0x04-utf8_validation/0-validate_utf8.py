64ffca26159a3abac59a378c9f2591d67339ea8a
noah-brunate <noahbrunate9@gmail.com>
2023-10-27 02:33:41 -0700

    utf-8 validation code

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+script defines a function validUTF8(data) that if agiven
+dataset represents a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """Function returns True if data represents
+       a valid UTF-8 encoding or False otherwise
+    """
+
+    # initialize expected bytes
+    num_bytes = 0
+
+    for num in data:
+        if num_bytes == 0:
+            if (num >> 7) == 0b0:
+                continue
+            elif (num >> 5) == 0b110:
+                num_bytes = 1
+            elif (num >> 4) == 0b1110:
+                num_bytes = 2
+            elif (num >> 3) == 0b11110:
+                num_bytes = 3
+            else:
+                return False
+        else:
+            if (num >> 6) != 0b10:
+                return False
+            num_bytes -= 1
+    return num_bytes == 0
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+# Solution to 0x04. UTF-8 Validation task
