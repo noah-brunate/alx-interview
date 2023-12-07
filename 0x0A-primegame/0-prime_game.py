9c6370a6f8a800d513dc51bf9b6f5b0e9a56e96d
noah-brunate <noahbrunate9@gmail.com>
2023-12-07 11:10:51 -0800

    primegame

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Module defines the isWinner() function"""
+
+
+def sieve_of_eratosthenes(n):
+    primes = []  # List to store the prime numbers
+    is_prime = [True] * (n+1)  # Initialize a list to mark numbers as Prime
+
+    # Mark all multiples of 2 as non-prime
+    for p in range(2, int(n**0.5) + 1):
+        if is_prime[p]:
+            primes.append(p)  # Add the prime number to the list
+            for i in range(p * p, n + 1, p):
+                is_prime[i] = False
+
+    # Add the remaining prime numbers to the list
+    for p in range(int(n**0.5) + 1, n + 1):
+        if is_prime[p]:
+            primes.append(p)
+
+    return primes
+
+
+def isWinner(x, nums):
+    """function returns a winner of the game"""
+
+    Maria = 0
+    Ben = 0
+
+    for i in nums:
+        if i == 0:
+            Ben += 1
+        if i < 0:
+            return None
+        primes = sieve_of_eratosthenes(i)
+        size = len(primes)
+        if size % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+    if Ben > Maria:
+        return 'Ben'
+    elif Ben < Maria:
+        return 'Maria'
+    else:
+        return None
